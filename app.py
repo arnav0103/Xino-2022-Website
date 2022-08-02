d66ab9ad09392da4fa01a3533e5fda3d7bcbb37a
arnav0103 <arnav.gupta0103@gmail.com>
2022-08-02 09:40:16 +0530

    a

--- app.py
+++ app.py
@@ -125,101 +125,101 @@ def register(school, hash):
         sheet_events.update_cell(
             next_events_row, 24, form.participant_cr5_name.data)
         sheet_events.update_cell(
-            next_events_row, 24, form.participant_cr5_email.data)
+            next_events_row, 25, form.participant_cr5_email.data)
         sheet_events.update_cell(
-            next_events_row, 25, form.participant_cr5_phone.data)
+            next_events_row, 26, form.participant_cr5_phone.data)
 
 
 # crossword
         sheet_events.update_cell(
-            next_events_row, 26, form.participant_cw1_name.data)
+            next_events_row, 27, form.participant_cw1_name.data)
         sheet_events.update_cell(
-            next_events_row, 27, form.participant_cw1_email.data)
+            next_events_row, 28, form.participant_cw1_email.data)
         sheet_events.update_cell(
-            next_events_row, 28, form.participant_cw1_phone.data)
+            next_events_row, 29, form.participant_cw1_phone.data)
 
         sheet_events.update_cell(
-            next_events_row, 29, form.participant_cw2_name.data)
+            next_events_row, 30, form.participant_cw2_name.data)
         sheet_events.update_cell(
-            next_events_row, 30, form.participant_cw2_email.data)
+            next_events_row, 31, form.participant_cw2_email.data)
         sheet_events.update_cell(
-            next_events_row, 31, form.participant_cw2_phone.data)
+            next_events_row, 32, form.participant_cw2_phone.data)
 
 # prog
         sheet_events.update_cell(
-            next_events_row, 32, form.participant_pg1_name.data)
+            next_events_row, 33, form.participant_pg1_name.data)
         sheet_events.update_cell(
-            next_events_row, 33, form.participant_pg1_email.data)
+            next_events_row, 34, form.participant_pg1_email.data)
         sheet_events.update_cell(
-            next_events_row, 34, form.participant_pg1_phone.data)
+            next_events_row, 35, form.participant_pg1_phone.data)
 
         sheet_events.update_cell(
-            next_events_row, 35, form.participant_pg2_name.data)
+            next_events_row, 36, form.participant_pg2_name.data)
         sheet_events.update_cell(
-            next_events_row, 36, form.participant_pg2_email.data)
+            next_events_row, 37, form.participant_pg2_email.data)
         sheet_events.update_cell(
-            next_events_row, 37, form.participant_pg2_phone.data)
+            next_events_row, 38, form.participant_pg2_phone.data)
 
 # hardware
         sheet_events.update_cell(
-            next_events_row, 38, form.participant_hr1_name.data)
+            next_events_row, 39, form.participant_hr1_name.data)
         sheet_events.update_cell(
-            next_events_row, 39, form.participant_hr1_email.data)
+            next_events_row, 40, form.participant_hr1_email.data)
         sheet_events.update_cell(
-            next_events_row, 40, form.participant_hr1_phone.data)
+            next_events_row, 41, form.participant_hr1_phone.data)
 
         sheet_events.update_cell(
-            next_events_row, 41, form.participant_hr2_name.data)
+            next_events_row, 42, form.participant_hr2_name.data)
         sheet_events.update_cell(
-            next_events_row, 42, form.participant_hr2_email.data)
+            next_events_row, 43, form.participant_hr2_email.data)
         sheet_events.update_cell(
-            next_events_row, 43, form.participant_hr2_phone.data)
+            next_events_row, 44, form.participant_hr2_phone.data)
 
         sheet_events.update_cell(
-            next_events_row, 44, form.participant_hr3_name.data)
+            next_events_row, 45, form.participant_hr3_name.data)
         sheet_events.update_cell(
-            next_events_row, 45, form.participant_hr3_email.data)
+            next_events_row, 46, form.participant_hr3_email.data)
         sheet_events.update_cell(
-            next_events_row, 46, form.participant_hr3_phone.data)
+            next_events_row, 47, form.participant_hr3_phone.data)
 
 
 # game dev
         sheet_events.update_cell(
-            next_events_row, 47, form.participant_gm1_name.data)
+            next_events_row, 48, form.participant_gm1_name.data)
         sheet_events.update_cell(
-            next_events_row, 48, form.participant_gm1_email.data)
+            next_events_row, 49, form.participant_gm1_email.data)
         sheet_events.update_cell(
-            next_events_row, 49, form.participant_gm1_phone.data)
+            next_events_row, 50, form.participant_gm1_phone.data)
 
         sheet_events.update_cell(
-            next_events_row, 50, form.participant_gm2_name.data)
+            next_events_row, 51, form.participant_gm2_name.data)
         sheet_events.update_cell(
-            next_events_row, 51, form.participant_gm2_email.data)
+            next_events_row, 52, form.participant_gm2_email.data)
         sheet_events.update_cell(
-            next_events_row, 52, form.participant_gm2_phone.data)
+            next_events_row, 53, form.participant_gm2_phone.data)
 
         sheet_events.update_cell(
-            next_events_row, 53, form.participant_gm3_name.data)
+            next_events_row, 54, form.participant_gm3_name.data)
         sheet_events.update_cell(
-            next_events_row, 54, form.participant_gm3_email.data)
+            next_events_row, 55, form.participant_gm3_email.data)
         sheet_events.update_cell(
-            next_events_row, 55, form.participant_gm3_phone.data)
+            next_events_row, 56, form.participant_gm3_phone.data)
 
 # musical shutter
         sheet_events.update_cell(
-            next_events_row, 56, form.participant_ms1_name.data)
+            next_events_row, 57, form.participant_ms1_name.data)
         sheet_events.update_cell(
-            next_events_row, 57, form.participant_ms1_email.data)
+            next_events_row, 58, form.participant_ms1_email.data)
         sheet_events.update_cell(
-            next_events_row, 58, form.participant_ms1_phone.data)
+            next_events_row, 59, form.participant_ms1_phone.data)
 
 # camcord
         sheet_events.update_cell(
-            next_events_row, 59, form.participant_cc1_name.data)
+            next_events_row, 60, form.participant_cc1_name.data)
         sheet_events.update_cell(
-            next_events_row, 60, form.participant_cc1_email.data)
+            next_events_row, 61, form.participant_cc1_email.data)
         sheet_events.update_cell(
-            next_events_row, 61, form.participant_cc1_phone.data)
+            next_events_row, 62, form.participant_cc1_phone.data)
 
     return render_template('register.htm', form=form, sheet_events=sheet_events, next_events_row=next_events_row)
 
